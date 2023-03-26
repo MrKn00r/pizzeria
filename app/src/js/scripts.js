ea993dedf21e4a8a6f68129885e5fdcc1a7f957a
MrKn00r <83165070+MrKn00r@users.noreply.github.com>
2023-03-26 20:58:09 +0200

    create slider (no images)

--- app/src/js/scripts.js
+++ app/src/js/scripts.js
@@ -1,10 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn');
 const mobileMenu = document.querySelector('.mobileMenu');
 const delivery = document.querySelector('.delivery');
+const slider = document.querySelector('.slider');
+
+
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     mobileMenu.classList.toggle("active");
     delivery.classList.toggle("active");
+    slider.classList.toggle("active");
+    
 
 })
